fd27d1208f61f7b4da3367c2ef69f24be3bb0645
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2020-01-09 17:04:42 -0500

    Force localhost to avoid cors errors on IP

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 
 module.exports = {
   devServer: {
+    host: "localhost",
     https: {
       key: fs.readFileSync("./certs/localhost.key"),
       cert: fs.readFileSync("./certs/localhost.cert")
